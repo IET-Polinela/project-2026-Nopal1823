971701018de13f44b106d3c1c9529c7739171c26
nopalpriasigma <naufalalfathanhafizh@gmail.com>
2026-03-15 23:14:04 +0700

    LabSession_2
    Membuat 3 APP:
    - main_app
    - about
    - contacts

---
+++ npm24782088_iet_2026/npm24782088_iet_2026/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.http import HttpResponse
+
+def welcome(request):
+    return HttpResponse("Selamat Datang")
+
+urlpatterns = [
+    path('', include('main_app.urls')),
+    path('admin/', admin.site.urls),
+    path('welcome/', welcome),
+    path('about/', include('about.urls')),
+    path('contacts/', include('contacts.urls'))
+]
