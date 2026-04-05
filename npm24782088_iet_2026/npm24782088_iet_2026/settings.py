fbb5f93b281d97ae58f3130417d35877cf9eb5ab
nopalpriasigma <naufalalfathanhafizh@gmail.com>
2026-04-05 21:26:32 +0700

    LabSession_3

--- npm24782088_iet_2026/npm24782088_iet_2026/settings.py
+++ npm24782088_iet_2026/npm24782088_iet_2026/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'npm24782088_iet_2026.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'metrodb',
+        'USER': 'postgres',
+        'PASSWORD': '123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
